d82d49cc215a6b05c2cf54645171f58d634a27a3
Leo <balashov.leo@gmail.com>
2022-06-14 18:28:36 +0300

    Returning back

--- Assets/Scripts/EnemyBaseAI.cs
+++ Assets/Scripts/EnemyBaseAI.cs
@@ -8,7 +8,7 @@ public class EnemyBaseAI : EnemyTools
 {
     public float Speed;
     public float AttackDistance;
-    [SerializeField] private float _frequencyOfAttackChecking;
+    [SerializeField] private float _frequencyOfPathFinding;
     [SerializeField] private float _waitTime;
     [SerializeField] private float _peekNextWaypointDistance = 2f;
     private Transform _transform;
@@ -26,7 +26,7 @@ public class EnemyBaseAI : EnemyTools
         _transform = GetComponent<Transform>();
         _startPosition = _transform.position;
     }
-    
+
     private void Update()
     {
         if (_notBlocked
@@ -35,8 +35,6 @@ public class EnemyBaseAI : EnemyTools
             if (_currentWaypoint >= _path.vectorPath.Count)
             {
                 _path = null;
-                _player.DeleteEnemy(gameObject);
-                StartCoroutine(nameof(GoBack));
                 return;
             }
 
@@ -60,14 +58,16 @@ public class EnemyBaseAI : EnemyTools
         }
     }
 
-    public void TargetDetected(Transform target)
+    public void TargetDetected(GameObject target)
     {
-        _player.DeleteEnemy(gameObject);
         _player.AddEnemy(gameObject);
-        _target = target;
+        _target = target.GetComponent<Transform>();
         _currentWaypoint = 0;
-        StopCoroutine(nameof(BuildingPathWhileSee));
-        StartCoroutine(nameof(BuildingPathWhileSee));
+        if (_notBlocked)
+        {
+            StopCoroutine(nameof(BuildingPathWhileSee));
+            StartCoroutine(nameof(BuildingPathWhileSee));
+        }
     }
 
     private void PathCompleted(Path path)
@@ -79,24 +79,31 @@ public class EnemyBaseAI : EnemyTools
         }
     }
 
-    private IEnumerator CheckForAttackDistance()
+    private IEnumerator BuildingPathWhileSee()
     {
         float distanceToTarget = Vector2.Distance(_transform.position, _target.position);
 
-        while (distanceToTarget < 10)
+        while (distanceToTarget < 11)
         {
+            BuildPath(selfPosition: _transform.position,
+                        targetPosition: _target.position,
+                        callbackFunction: PathCompleted);
             if (_notBlocked
-                    && distanceToTarget < AttackDistance)
+                && distanceToTarget < AttackDistance)
             {
                 _transform.rotation = GetNewRotation(selfPosition: _transform.position,
                                                     targetPosition: _target.position);
                 Block();
                 Attack();
             }
-            yield return new WaitForSeconds(_frequencyOfAttackChecking);
+            yield return new WaitForSeconds(_frequencyOfPathFinding);
             distanceToTarget = Vector2.Distance(_transform.position, _target.position);
         }
-
+        _player.DeleteEnemy(gameObject);
+        yield return new WaitForSeconds(_waitTime);
+        BuildPath(selfPosition: _transform.position,
+                    targetPosition: _startPosition,
+                    callbackFunction: PathCompleted);
     }
 
     public void Block()
@@ -108,16 +115,8 @@ public class EnemyBaseAI : EnemyTools
 
     private IEnumerator Unblock()
     {
-        yield return new WaitForSeconds(_frequencyOfAttackChecking);
+        yield return new WaitForSeconds(_frequencyOfPathFinding);
         _notBlocked = true;
-    }
-
-    private IEnumerator GoBack()
-    {
-        yield return new WaitForSeconds(_waitTime);
-        BuildPath(selfPosition: _transform.position,
-                    targetPosition: _startPosition,
-                    callbackFunction: PathCompleted);
     }
 
     public override void Discard(Vector2 direction)
