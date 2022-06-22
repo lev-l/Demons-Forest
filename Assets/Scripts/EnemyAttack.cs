bdc30666502344229905f3af8032b5764b7d868d
Leo <balashov.leo@gmail.com>
2022-06-22 18:43:15 +0300

    Add speed controling system

--- Assets/Scripts/EnemyAttack.cs
+++ Assets/Scripts/EnemyAttack.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(EnemyBaseAI))]
 public class EnemyAttack : MonoBehaviour
 {
     public AnimationCurve AttackCurve;
