dec9181ef63c04e6c80166688397c973c064ebf6
Leo <balashov.leo@gmail.com>
2022-10-12 13:16:52 +0300

    Group vision basis

---
+++ Assets/Scripts/AI/GroupVision.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroupVision : MonoBehaviour
+{
+    private List<EnemyBaseAI> _group;
+
+    private void Start()
+    {
+        _group = new List<EnemyBaseAI>();
+        _group.AddRange(GetComponentsInChildren<EnemyBaseAI>());
+    }
+
+    public void Notify(GameObject target)
+    {
+        foreach(EnemyBaseAI member in _group)
+        {
+            member.TargetDetected(target);
+        }
+    }
+}
