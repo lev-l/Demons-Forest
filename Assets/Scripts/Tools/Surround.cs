43c0be40ce482c1086a8fa1d75ae5710daa2887b
Leo <balashov.leo@gmail.com>
2022-11-10 08:41:02 +0200

    Finding destinations around player
    Basic function!

--- Assets/Scripts/Tools/Surround.cs
+++ Assets/Scripts/Tools/Surround.cs
@@ -1,5 +1,14 @@
-﻿public class Surround
+﻿using UnityEngine;
+
+public class Surround
 {
+    private Trigonometric _angleFunctions;
+
+    public Surround()
+    {
+        _angleFunctions = new Trigonometric();
+    }
+
     public float[] FindAngles(float basicAngle, int numberOfAngles)
     {
         float angle = 360f / numberOfAngles;
@@ -7,9 +16,25 @@
         float[] directionsAngles = new float[numberOfAngles];
         for(int i = 0; i < numberOfAngles; i++)
         {
-            directionsAngles[i] = basicAngle + angle * i;
+            directionsAngles[i] = _angleFunctions.AddAngle(basicAngle, angle * i);
         }
 
         return directionsAngles;
+    }
+
+    public Vector2[] FindDestinations(float[] angles,
+                                float distanceFromTarget)
+    {
+        Vector2[] results = new Vector2[angles.Length];
+
+        for(int i = 0; i < angles.Length; i++)
+        {
+            results[i] = _angleFunctions.CreateRayEnd(distanceFromTarget, angles[i]);
+
+
+            _angleFunctions.RayPaint(Vector2.zero, results[i]);
+        }
+
+        return results;
     }
 }
