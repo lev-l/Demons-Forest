6af3ae6306c950fc8dea5b75df1419f5a9611475
Leo <balashov.leo@gmail.com>
2022-11-10 09:45:11 +0200

    Surrounding
    Delete logs

--- Assets/Scripts/Tools/Surround.cs
+++ Assets/Scripts/Tools/Surround.cs
@@ -2,13 +2,6 @@
 
 public class Surround
 {
-    private Trigonometric _angleFunctions;
-
-    public Surround()
-    {
-        _angleFunctions = new Trigonometric();
-    }
-
     public float[] FindAngles(float basicAngle, int numberOfAngles)
     {
         float angle = 360f / numberOfAngles;
@@ -16,7 +9,7 @@ public class Surround
         float[] directionsAngles = new float[numberOfAngles];
         for(int i = 0; i < numberOfAngles; i++)
         {
-            directionsAngles[i] = _angleFunctions.AddAngle(basicAngle, angle * i);
+            directionsAngles[i] = Trigonometric.AddAngle(basicAngle, angle * i);
         }
 
         return directionsAngles;
@@ -29,10 +22,7 @@ public class Surround
 
         for(int i = 0; i < angles.Length; i++)
         {
-            results[i] = _angleFunctions.CreateRayEnd(distanceFromTarget, angles[i]);
-
-
-            _angleFunctions.RayPaint(Vector2.zero, results[i]);
+            results[i] = Trigonometric.CreateRayEnd(distanceFromTarget, angles[i]);
         }
 
         return results;
