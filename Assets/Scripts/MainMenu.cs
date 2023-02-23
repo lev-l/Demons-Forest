03f5f675645143cbd2ce47c485391ad1c226233b
Leo <balashov.leo@gmail.com>
2023-02-23 15:37:40 +0200

    NewGameConfirmation

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,10 +1,37 @@
 using System.IO;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    private SaveNotice _confirmationMenu;
+
+    private void Start()
+    {
+        _confirmationMenu = FindObjectOfType<SaveNotice>(true);
+
+        if(!File.Exists(Application.dataPath + "/MainSave.add")
+            && !File.Exists(Application.dataPath + "/MainSave"))
+        {
+            GameObject.Find("Continue").GetComponent<Button>().interactable = false;
+        }
+    }
+
     public void NewGame()
+    {
+        if(File.Exists(Application.dataPath + "/MainSave")
+            || File.Exists(Application.dataPath + "/MainSave.add"))
+        {
+            _confirmationMenu.Show();
+        }
+        else
+        {
+            StartNewGame();
+        }
+    }
+
+    public void StartNewGame()
     {
         string path = Application.dataPath + "/";
         File.Delete(path + "MainSave");
