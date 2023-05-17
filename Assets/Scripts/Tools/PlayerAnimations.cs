83aca16c28023542f52a27154f88d07af826fde3
Lev <balashov.leo@gmail.com>
2023-05-17 09:13:36 +0300

    Steps Sound

--- Assets/Scripts/Tools/PlayerAnimations.cs
+++ Assets/Scripts/Tools/PlayerAnimations.cs
@@ -5,17 +5,33 @@ using UnityEngine;
 public class PlayerAnimations : MonoBehaviour
 {
     private Animator _animator;
+    private AudioSource _stepsSound;
+    private bool _wasMoving = false;
 
     private void Start()
     {
         _animator = GetComponentInChildren<Animator>();
+        _stepsSound = GetComponent<AudioSource>();
 
         Resources.Load<PlayerObject>("Player").OnStealthChanged += SetStealth;
     }
 
     public void ChangeRunState(Vector2 move)
     {
-        _animator.SetBool("Run", move.sqrMagnitude > 0);
+        bool isMoving = move.sqrMagnitude > 0;
+        bool change = isMoving != _wasMoving;
+        _wasMoving = isMoving;
+
+        _animator.SetBool("Run", isMoving);
+
+        if (change && isMoving)
+        {
+            _stepsSound.Play();
+        }
+        else if (change)
+        {
+            _stepsSound.Pause();
+        }
     }
 
     public void PrepareAttackAnimation()
