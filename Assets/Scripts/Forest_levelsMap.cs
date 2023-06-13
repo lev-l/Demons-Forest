f8ce8ed0475eda873720a07ad252d3393155edd8
Lev <balashov.leo@gmail.com>
2023-06-13 12:34:13 +0300

    StartLevel-Iteration1

--- Assets/Scripts/Forest_levelsMap.cs
+++ Assets/Scripts/Forest_levelsMap.cs
@@ -4,8 +4,8 @@ struct Forest_levelsMap
 {
     public string[,] GetMap()
     {
-        string[,] map = new string[1, 3] {
-            { "ForestT_0", "ForestT_1", "ForestT_2" }
+        string[,] map = new string[,] {
+            { "ForestD_0-0" }
         };
 
         return map;
