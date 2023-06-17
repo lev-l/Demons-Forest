b3de5ddfaf7c6ee7825fa1b16e59a10f3ff9a043
Lev <balashov.leo@gmail.com>
2023-06-17 16:32:40 +0300

    FirstRoadLevel

--- Assets/Scripts/Forest_levelsMap.cs
+++ Assets/Scripts/Forest_levelsMap.cs
@@ -5,7 +5,7 @@ struct Forest_levelsMap
     public string[,] GetMap()
     {
         string[,] map = new string[,] {
-            { "ForestD_0-0" }
+            { "ForestD_0-0", "ForestD_0-1" }
         };
 
         return map;
