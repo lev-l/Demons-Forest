a0f70593e39a61834c18011738e858edbfb45e40
Lev <balashov.leo@gmail.com>
2023-06-18 15:39:45 +0300

    Auto Grass Placement

--- Assets/Editor/AutoHesh.cs
+++ Assets/Editor/AutoHesh.cs
@@ -3,46 +3,38 @@ using UnityEditor;
 
 public class AutoHesh : ScriptableWizard
 {
-    public string Scene;
-
     [MenuItem ("Tools/Auto Heshing")]
     private static void ShowAutoHeshWindow()
     {
-        ScriptableWizard.DisplayWizard<AutoHesh>("Auto Heshing", "HeshEnemies", "HeshChests");
-    }
+        string activeScene = EditorSceneManager.GetActiveScene().name;
 
-    private void OnWizardCreate()
-    {
         GroupForm[] enemyGroups = FindObjectsOfType<GroupForm>();
 
         for (int i = 0; i < enemyGroups.Length; i++)
         {
-            enemyGroups[i].Hesh = EditorSceneManager.GetActiveScene().name;
+            enemyGroups[i].Hesh = activeScene;
             if (i > 0)
             {
                 enemyGroups[i].Hesh += $"({i})";
             }
         }
 
-        EnemyUnload[] enemies = FindObjectsOfType<EnemyUnload>();
-
-        foreach(EnemyUnload enemy in enemies)
-        {
-            enemy.Origin = EditorSceneManager.GetActiveScene().name;
-        }
-    }
-
-    private void OnWizardOtherButton()
-    {
         Chest[] chests = FindObjectsOfType<Chest>();
 
         for (int i = 0; i < chests.Length; i++)
         {
-            chests[i].Hesh = EditorSceneManager.GetActiveScene().name;
+            chests[i].Hesh = activeScene;
             if (i > 0)
             {
                 chests[i].Hesh += $"({i})";
             }
+        }
+
+        EnemyUnload[] enemies = FindObjectsOfType<EnemyUnload>();
+
+        foreach (EnemyUnload enemy in enemies)
+        {
+            enemy.Origin = activeScene;
         }
     }
 }
