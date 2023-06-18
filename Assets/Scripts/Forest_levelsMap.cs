2d4e11243d7a4aec896c880534767a7a00a4732b
Lev <balashov.leo@gmail.com>
2023-06-18 11:50:33 +0300

    LevelsBlueprints0.0-9.1

--- Assets/Scripts/Forest_levelsMap.cs
+++ Assets/Scripts/Forest_levelsMap.cs
@@ -5,7 +5,16 @@ struct Forest_levelsMap
     public string[,] GetMap()
     {
         string[,] map = new string[,] {
-            { "ForestD_0-0", "ForestD_0-1" }
+            { "ForestD_0-0", "ForestD_0-1" },
+            { "ForestD_1-0", "ForestD_1-1" },
+            { "ForestD_2-0", "ForestD_2-1" },
+            { "ForestD_3-0", "ForestD_3-1" },
+            { "ForestD_4-0", "ForestD_4-1" },
+            { "ForestD_5-0", "ForestD_5-1" },
+            { "ForestD_6-0", "ForestD_6-1" },
+            { "ForestD_7-0", "ForestD_7-1" },
+            { "ForestD_8-0", "ForestD_8-1" },
+            { "ForestD_9-0", "ForestD_9-1" }
         };
 
         return map;
