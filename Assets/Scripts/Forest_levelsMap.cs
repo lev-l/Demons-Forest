7f3cc81faa0af92c4b7d9cb224b5abedf62a533d
Lev <balashov.leo@gmail.com>
2023-07-26 11:27:40 +0300

    Road

--- Assets/Scripts/Forest_levelsMap.cs
+++ Assets/Scripts/Forest_levelsMap.cs
@@ -5,16 +5,16 @@ struct Forest_levelsMap
     public string[,] GetMap()
     {
         string[,] map = new string[,] {
-            { "ForestD_0-0", "ForestD_0-1" },
-            { "ForestD_1-0", "ForestD_1-1" },
-            { "ForestD_2-0", "ForestD_2-1" },
-            { "ForestD_3-0", "ForestD_3-1" },
-            { "ForestD_4-0", "ForestD_4-1" },
-            { "ForestD_5-0", "ForestD_5-1" },
-            { "ForestD_6-0", "ForestD_6-1" },
-            { "ForestD_7-0", "ForestD_7-1" },
-            { "ForestD_8-0", "ForestD_8-1" },
-            { "ForestD_9-0", "ForestD_9-1" }
+            { "ForestD_0-0", "ForestD_0-1", "ForestD_0-2", "ForestD_0-3", "ForestD_0-4", "ForestD_0-5", "ForestD_0-6", },
+            { "ForestD_1-0", "ForestD_1-1", "ForestD_1-2", "ForestD_1-3", "ForestD_1-4", "ForestD_1-5", "ForestD_1-6", },
+            { "ForestD_2-0", "ForestD_2-1", "ForestD_2-2", "ForestD_2-3", "ForestD_2-4", "ForestD_2-5", "ForestD_2-6", },
+            { "ForestD_3-0", "ForestD_3-1", "ForestD_3-2", "ForestD_3-3", "ForestD_3-4", "ForestD_3-5", "ForestD_3-6", },
+            { "ForestD_4-0", "ForestD_4-1", "ForestD_4-2", "ForestD_4-3", "ForestD_4-4", "ForestD_4-5", "ForestD_4-6", },
+            { "ForestD_5-0", "ForestD_5-1", "ForestD_5-2", "ForestD_5-3", "ForestD_5-4", "ForestD_5-5", "ForestD_5-6", },
+            { "ForestD_6-0", "ForestD_6-1", "ForestD_6-2", "ForestD_6-3", "ForestD_6-4", "ForestD_6-5", "ForestD_6-6", },
+            { "ForestD_7-0", "ForestD_7-1", "ForestD_7-2", "ForestD_7-3", "ForestD_7-4", "ForestD_7-5", "ForestD_7-6", },
+            { "ForestD_8-0", "ForestD_8-1", "ForestD_8-2", "ForestD_8-3", "ForestD_8-4", "ForestD_8-5", "ForestD_8-6", },
+            { "ForestD_9-0", "ForestD_9-1", "ForestD_9-2", "ForestD_9-3", "ForestD_9-4", "ForestD_9-5", "ForestD_9-6" }
         };
 
         return map;
